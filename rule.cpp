f3e7028edd6ad6c85c0be10873123c685f6401f1
sa8 <sarahazouvi@mbp-de-sarah.home>
2015-04-14 10:51:09 +0200

    Change : the board doesn't have to be a square, it can be a rectangle

--- rule.cpp
+++ rule.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 Rule::Rule(){
-    m_sizegrid=100;
-    m_current.resize(m_sizegrid+2);
-    for (int l=0;l<(m_sizegrid+2);l++) m_current[l].resize(m_sizegrid+2);
+    m_sizeX=100;
+    m_sizeY=100;
+    m_current.resize(m_sizeX+2);
+    for (int l=0;l<(m_sizeX+2);l++) m_current[l].resize(m_sizeY+2);//increase the size of the "board" to make it easier to calculate states (don't need to differentiate edge or not edge)
     
-    for (int i=0; i<=m_sizegrid+1;i++){
-        m_current[0][i]=m_current[i][0]=0;
-        m_current[m_sizegrid+1][i]=m_current[i][m_sizegrid+1]=0;
-        
+    for (int i=0; i<=m_sizeX+1;i++){
+         m_current[i][0]=m_current[i][m_sizeY+1]=0;}
+    for (int i=0; i<=m_sizeY+1;i++){
+        m_current[m_sizeX+1][i]=m_current[0][i]=0;
     }
-    for (int i=1;i<=m_sizegrid;i++){
-        for (int j=1;j<=m_sizegrid;j++){
-            m_current[i][j]=rand()%2;
+    
+    for (int i=1;i<=m_sizeX;i++){
+        for (int j=1;j<=m_sizeY;j++){
+            m_current[i][j]=rand()%2; //by default create a random initial state
         }
         
     }
@@ -21,17 +23,18 @@ Rule::Rule(){
     
 }
 
-Rule::Rule(int size, std::vector< std::vector< int > > init){
-    m_sizegrid=size;
-    m_current.resize(m_sizegrid+2);
-    for (int l=0;l<(m_sizegrid+2);l++) m_current[l].resize(m_sizegrid+2);
-    for (int i=0; i<=m_sizegrid+1;i++){
-        m_current[0][i]=m_current[i][0]=0;
-        m_current[m_sizegrid+1][i]=m_current[i][m_sizegrid+1]=0;
-        
+Rule::Rule(int sizeX,int sizeY, std::vector< std::vector< int > > init){
+    m_sizeX=sizeX;
+    m_sizeY=sizeY;
+    m_current.resize(m_sizeX+2);
+    for (int l=0;l<(m_sizeX+2);l++) m_current[l].resize(m_sizeY+2);
+    for (int i=0; i<=m_sizeX+1;i++){
+        m_current[i][0]=m_current[i][m_sizeY+1]=0;}
+    for (int i=0; i<=m_sizeY+1;i++){
+        m_current[m_sizeX+1][i]=m_current[0][i]=0;
     }
-    for (int i=1;i<=m_sizegrid;i++){
-        for (int j=1;j<=m_sizegrid;j++){
+    for (int i=1;i<=m_sizeX;i++){
+        for (int j=1;j<=m_sizeY;j++){
             m_current[i][j]=init[i][j];
         }
         
@@ -41,10 +44,10 @@ Rule::Rule(int size, std::vector< std::vector< int > > init){
 }
 
 vector< vector< int> > Rule::getState(){
-    vector< vector<int> > board(m_sizegrid, vector<int>(m_sizegrid));
-    for (int i=1;i<=m_sizegrid;i++){
-        for (int j=1;j<=m_sizegrid;j++){
-            board[i-1][j-1]=m_current[i][j];
+    vector< vector<int> > board(m_sizeX, vector<int>(m_sizeY));
+    for (int i=1;i<=m_sizeX;i++){
+        for (int j=1;j<=m_sizeY;j++){
+            board[i-1][j-1]=m_current[i][j];//get the current state without the "edges" that I made to make my code easier
         }
         
     }
@@ -55,10 +58,10 @@ vector< vector< int> > Rule::getState(){
 
 
 void Rule::NewState(){
-     std::vector<std::vector<int > > m_new(m_sizegrid+2,vector<int>(m_sizegrid+2)) ;
+     std::vector<std::vector<int > > m_new(m_sizeX+2,vector<int>(m_sizeY+2)) ;
     
-    for (int i=1;i<=m_sizegrid;i++){
-        for (int j=1;j<=m_sizegrid;j++){
+    for (int i=1;i<=m_sizeX;i++){
+        for (int j=1;j<=m_sizeY;j++){
             int sum= m_current[i-1][j-1]+m_current[i-1][j]+m_current[i-1][j+1]+m_current[i][j-1]+m_current[i][j+1]+m_current[i+1][j-1]+m_current[i+1][j]+m_current[i+1][j+1]+m_current[i][j];
             if (sum==3) m_new[i][j]=1;
             else if (sum==4) m_new[i][j]=m_current[i][j];
@@ -66,11 +69,13 @@ void Rule::NewState(){
         }
         
     }
-    for (int i=0; i<=m_sizegrid+1;i++){
-        m_new[0][i]=m_new[i][0]=0;
-        m_new[m_sizegrid+1][i]=m_new[i][m_sizegrid+1]=0;
+    for (int i=0; i<=m_sizeX+1;i++){
+        m_new[i][0]=m_new[i][m_sizeY+1]=0;
         
     }
+    for (int i=0; i<=m_sizeY+1;i++){
+        m_new[0][i]=m_new[m_sizeX+1][i]=0;}
+    
     m_current.clear();
     m_current=m_new;
     m_new.clear();
