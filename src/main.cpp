6a2fce9121a67b293131d27c8d3cef4d6710f9f9
sa8 <sarahazouvi@MBP-de-Sarah.default>
2015-04-23 10:48:17 +0100

    add a comparison between serial and parallel implementation to the main

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,24 @@
 #include <iostream>
 #include "rule.h"
 #include <fstream>
+#include <ctime>
+#include <time.h>
+#include <sys/timeb.h>
 using namespace std;
 int ITERATION(10);
 
+inline long getCurrentTime() { //function to get the time in millisecond (not relevant otherwise)
+    timeb timebstr;
+    ftime( &timebstr );
+    return (long)(timebstr.time)*1000 + timebstr.millitm;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    // Obtain an output file that describes the state of the game:
     ofstream out("./output.txt");
     Rule game;
     for (int l=0; l<ITERATION; l++) {
         game.NewState();
+        //game.NewStateParallel();
         vector<vector<int> > board =game.getState();
         for (int i=0;i<board.size();i++){
             for (int j=0;j<board.size();j++){
@@ -20,6 +29,31 @@ int main(int argc, const char * argv[]) {
             out<<'\n';
         }
         
+    }
+    
+    //Code to obtain a comparison between parallel and serial code :
+    ofstream table("./time-comparison.csv");
+    table<<"Grid Size"<<","<<"Serial Time (ms)"<<","<<"Parallel Time (ms)"<<","<<"Difference Serial-Parallel (ms)"<<endl;
+    for (int i=100; i<=1000; i+=50){
+        cout<<"size of grid: "<<i<<'\t';
+        Rule game(i,i);
+        long start_time1=getCurrentTime();
+        for (int j=0; j<ITERATION*10;j++){
+            game.NewState();
+        }
+        long end_time1=getCurrentTime();
+        long time_elapsed1=end_time1-start_time1;
+    
+        cout <<"time in millisecond for serial implementation: "<<time_elapsed1<<'\t';
+        long start_time2=getCurrentTime();
+        for (int j=0; j<ITERATION*10;j++){
+            game.NewStateParallel();
+        }
+        long end_time2=getCurrentTime();
+        long time_elapsed2=end_time2-start_time2;
+        table<<i<<","<<time_elapsed1<<","<<time_elapsed2<<","<<time_elapsed1-time_elapsed2<<endl;
+        cout <<"for parallel implementation: "<<time_elapsed2<<'\t';
+        cout<<"Difference between serial and parallel: "<<time_elapsed1-time_elapsed2<<endl;
     }
     
     return 0;
